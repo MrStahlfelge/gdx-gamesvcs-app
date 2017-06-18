1d58a31cb90918dcfa99f6a6d586a44b4c21f5b3
Benjamin Schulte <lightblocks@golfgl.de>
2017-06-18 11:58:23 +0200

    Newgrounds.io API included and instantiated
    See http://www.newgrounds.com/projects/games/1110754/preview

--- html/src/de/golfgl/gdxgamesvcsapp/client/HtmlLauncher.java
+++ html/src/de/golfgl/gdxgamesvcsapp/client/HtmlLauncher.java
@@ -3,19 +3,35 @@ package de.golfgl.gdxgamesvcsapp.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import de.golfgl.gdxgamesvcs.NgioClient;
 import de.golfgl.gdxgamesvcsapp.GdxGameSvcsApp;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                GwtApplicationConfiguration config = new GwtApplicationConfiguration(480, 320);
-                config.preferFlash = false;
-                return config;
-        }
+    public static final String NG_APP_ID = "46188:ARRyvuAv";
+    public static final String NGIO_SESSIONID_PARAM = "ngio_session_id";
+    //http://www.newgrounds.com/projects/games/1110754/preview
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new GdxGameSvcsApp();
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        GwtApplicationConfiguration config = new GwtApplicationConfiguration(480, 320);
+        config.preferFlash = false;
+        return config;
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        GdxGameSvcsApp gdxGameSvcsApp = new GdxGameSvcsApp();
+        NgioClient gsClient = new NgioClient();
+
+        gsClient.initialize(NG_APP_ID,
+                com.google.gwt.user.client.Window.Location.getParameter(NGIO_SESSIONID_PARAM),
+                null);
+
+        gdxGameSvcsApp.gsClient = gsClient;
+
+        return gdxGameSvcsApp;
+    }
+
 }
