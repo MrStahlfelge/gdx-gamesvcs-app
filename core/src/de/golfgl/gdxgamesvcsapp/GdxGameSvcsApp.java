c067ed5446340a0e70a478ff7c12da3572267c97
Benjamin Schulte <lightblocks@golfgl.de>
2017-08-18 21:38:09 +0200

    Link to repo added, fetch leaderboard related to player added

--- core/src/de/golfgl/gdxgamesvcsapp/GdxGameSvcsApp.java
+++ core/src/de/golfgl/gdxgamesvcsapp/GdxGameSvcsApp.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
@@ -15,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
@@ -31,6 +33,7 @@ public class GdxGameSvcsApp extends ApplicationAdapter implements IGameServiceLi
     public static final String LEADERBOARD1 = "BOARD1";
     public static final String ACHIEVEMENT1 = "ACH1";
     public static final String EVENT1 = "EVENT1";
+    public static final String REPOLINK = "https://github.com/MrStahlfelge/gdx-gamesvcs";
 
     public IGameServiceClient gsClient;
     Skin skin;
@@ -92,6 +95,15 @@ public class GdxGameSvcsApp extends ApplicationAdapter implements IGameServiceLi
         gsUsername = new Label("", skin);
         scoreFillin = new TextField("100", skin);
 
+        Label repoLink = new Label(REPOLINK, skin);
+        repoLink.setColor(.3f, .3f, 1f, 1f);
+        repoLink.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.net.openURI(REPOLINK);
+            }
+        });
+
         signInButton = new TextButton("", skin);
         signInButton.addListener(new ChangeListener() {
             public void changed(ChangeEvent event, Actor actor) {
@@ -117,22 +129,7 @@ public class GdxGameSvcsApp extends ApplicationAdapter implements IGameServiceLi
         fetchLeaderboards.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                final MyDialog dialog = new MyDialog("Leaderboard");
-                boolean fetchingNow = gsClient.fetchLeaderboardEntries(LEADERBOARD1, 8, false,
-                        new IFetchLeaderBoardEntriesResponseListener() {
-                            @Override
-                            public void onLeaderBoardResponse(final Array<ILeaderBoardEntry> leaderBoard) {
-                                Gdx.app.postRunnable(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        showLeaderBoardEntries(dialog, leaderBoard);
-                                    }
-                                });
-                            }
-                        });
-
-                dialog.text(fetchingNow ? "Fetching..." : "Could not fetch");
-                dialog.show(stage);
+                fetchLeaderboard(false);
             }
         });
         fetchLeaderboards.setVisible(
@@ -215,7 +212,9 @@ public class GdxGameSvcsApp extends ApplicationAdapter implements IGameServiceLi
         table.defaults().pad(5);
         stage.addActor(table);
 
-        table.add(new Label("Gdx-GameServices Demo App", skin)).padBottom(20).colspan(3);
+        table.add(new Label("Gdx-GameServices Demo App", skin)).colspan(3).padBottom(0);
+        table.row();
+        table.add(repoLink).padBottom(20).colspan(3);
         table.row();
 
         table.add(new Label("Game Service ID:", skin)).right();
@@ -263,6 +262,37 @@ public class GdxGameSvcsApp extends ApplicationAdapter implements IGameServiceLi
         table.add(new Label(EVENT1, skin));
         table.add(submitEvent1Btn);
 
+    }
+
+    private void fetchLeaderboard(boolean playerRelated) {
+        final MyDialog dialog = new MyDialog("Leaderboard");
+
+        if (!playerRelated) {
+            TextButton nowPlayer = new TextButton("Player related", skin);
+            nowPlayer.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    fetchLeaderboard(true);
+                }
+            });
+            dialog.button(nowPlayer);
+        }
+
+        boolean fetchingNow = gsClient.fetchLeaderboardEntries(LEADERBOARD1, 8, playerRelated,
+                new IFetchLeaderBoardEntriesResponseListener() {
+                    @Override
+                    public void onLeaderBoardResponse(final Array<ILeaderBoardEntry> leaderBoard) {
+                        Gdx.app.postRunnable(new Runnable() {
+                            @Override
+                            public void run() {
+                                showLeaderBoardEntries(dialog, leaderBoard);
+                            }
+                        });
+                    }
+                });
+
+        dialog.text(fetchingNow ? "Fetching..." : "Could not fetch");
+        dialog.show(stage);
     }
 
     private void showAchievementsList(MyDialog dialog, Array<IAchievement> achievements) {
